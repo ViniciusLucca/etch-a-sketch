3b8cae201779c242fa0c43cfda78f13287c65907
ViniciusLucca <vinilucca7304@gmail.com>
2025-03-17 17:59:01 -0300

    refactor: Update 'create grid' feature
    Now the feature can be used from a named function, with an argument for the size of the grid

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const gridContainer = document.querySelector('.grid-container');
 const gridItem = document.createElement('div');
+// This stylesheet is necessary to adjust the items size
+const styleSheet = document.createElement('style');
 gridItem.setAttribute('class', 'grid-item');
 
-// Creating a 16x16 grid results in 256 items, because 16*16 = 256
-for (let index = 0; index < 256; index++) {
-    gridContainer.appendChild(gridItem.cloneNode());
+function createGridItems(gridSize){
+    let totalItems = gridSize*gridSize;
+    // Input validation
+    if (gridSize > 100 || gridSize < 1){
+        alert("Size invalid (must be between 1 and 100).");
+        return;
+    }
+    /*  This calculates the size the items 
+        should occupy in the container
+    */
+    let itemsSize = 100/gridSize;
+    // Set how many items should be in a line before wrapping
+    
+    let gridItemStyle = `.grid-item{ flex-basis: ${itemsSize}%; }`;
+    styleSheet.textContent = gridItemStyle;
+    document.head.appendChild(styleSheet);
+
+    for (let index = 0; index < totalItems; index++) {
+        gridContainer.appendChild(gridItem.cloneNode());
+    }
 }
 
 // Painting feature 
@@ -16,3 +35,5 @@ gridContainer.addEventListener('mouseover', paintBlock)
 function paintBlock(event){
     event.target.setAttribute('style', 'background-color: aquamarine');
 }
+
+createGridItems(16);
